a444f708f414649e32d03ac17a12b3d52e85bdd1
seijiro <modeverv@gmail.com>
2021-12-10 12:57:26 +0900

    2021-12-10 12:57:26

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,29 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var diceLeft = 1;
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+                onPressed: () {
+                  print("left button pressed");
+                  diceLeft = 2;
+                },
+                child: Image.asset('images/dice$diceLeft.png')),
+          ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+                onPressed: () {
+                  print("right button pressed");
+                },
+                child: Image.asset('images/dice2.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
